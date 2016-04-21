76dfc0b14b9df4656170c6825f652f137f9a3829
Cando <rpalma2609@gmail.com>
2016-04-20 20:43:20 -0500

    Proceso

---
+++ proceso.cs
@@ -0,0 +1,29 @@
+public int guardar(int dni,String apellido,String nombre,String direccion, int telefono, String fecha,String email,String sexo)
+        {
+
+            try
+            {
+                Conexion con = new Conexion();
+                con.Conectar();
+                con.CrearComando("INSERT INTO DATOS VALUES(?dni,?apellido,?nombre,?direccion,?telefono,?fecha,?email,?sexo)");
+                con.AsignarParametro("?dni",MySqlDbType.Int64,dni);
+                con.AsignarParametro("?apellido",MySqlDbType.String,apellido);
+                con.AsignarParametro("?nombre", MySqlDbType.String, nombre);
+                con.AsignarParametro("?direccion",MySqlDbType.String,direccion);
+                con.AsignarParametro("?telefono", MySqlDbType.Int64, telefono);
+                con.AsignarParametro("?fecha", MySqlDbType.String, fecha);
+                con.AsignarParametro("?email", MySqlDbType.String, email);
+                con.AsignarParametro("?sexo", MySqlDbType.String, sexo);
+                int numReg = 0;
+                numReg = con.EjecutarConsulta();
+                con.Desconectar();
+               return numReg;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Proceso Guardar: " + ex.ToString(), "Error");
+                return 0;
+            }
+
+        }
